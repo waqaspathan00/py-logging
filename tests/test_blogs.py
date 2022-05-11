c1e03d89b8b74482fdf0512b7f268070e57f5412
Abhishek Ranpura <acr35@njit.edu>
2022-05-11 19:19:32 -0400

    created blog tests

---
+++ tests/test_blogs.py
@@ -0,0 +1,28 @@
+import random
+
+
+def test_create_blog(client):
+    """ test creating a blog with filler post req"""
+    response = client.post("/", data={
+        "blog-comment": "this is blog post #" + str(random.randint(1, 1000000)),
+        "user-id": 0,
+        "owner": "tester@gmail.com"
+    })
+    data = response.data.decode("utf-8")
+    assert "Blog added!" in data
+
+
+def test_view_blogs(client):
+    """ test blogs are being saved """
+    from app.models import Blog
+    blogs = Blog.query.all()
+    assert blogs
+
+
+def test_delete_blogs(client):
+    from app.models import Blog
+    from app import db
+    db.session.query(Blog).delete()
+    db.session.commit()
+    blogs = Blog.query.all()
+    assert not blogs
--- tests/test_view.py
+++ tests/test_view.py
@@ -1,34 +1,22 @@
 """ testing viewable routes """
 
+
 def test_home_visitable(client):
     """ test that the home page is visible """
-    # ARRANGE - using client fixture
-
-    # ACT
     response = client.get("/")
     data = response.data.decode("utf-8")
+    assert "Blogs" in data
 
-    # ASSERT
-    assert "Search for a recipe!" in data
 
 def test_login_visitable(client):
     """ test that the home page is visible """
-    # ARRANGE - using client fixture
-
-    # ACT
     response = client.get("/login")
     data = response.data.decode("utf-8")
-
-    # ASSERT
     assert "Login" in data
+
 
 def test_sign_up_visitable(client):
     """ test that the home page is visible """
-    # ARRANGE - using client fixture
-
-    # ACT
     response = client.get("/sign-up")
     data = response.data.decode("utf-8")
-
-    # ASSERT
-    assert "Sign up" in data
+    assert "Sign Up" in data
