2f317027c18c87aeaf68843a09dd20d8d9f17744
Abhishek Ranpura <acr35@njit.edu>
2022-05-11 18:31:48 -0400

    testing ability to view pages

--- tests/test_view.py
+++ tests/test_view.py
@@ -9,4 +9,26 @@ def test_home_visitable(client):
     data = response.data.decode("utf-8")
 
     # ASSERT
-    assert "Home" in data
+    assert "Search for a recipe!" in data
+
+def test_login_visitable(client):
+    """ test that the home page is visible """
+    # ARRANGE - using client fixture
+
+    # ACT
+    response = client.get("/login")
+    data = response.data.decode("utf-8")
+
+    # ASSERT
+    assert "Login" in data
+
+def test_sign_up_visitable(client):
+    """ test that the home page is visible """
+    # ARRANGE - using client fixture
+
+    # ACT
+    response = client.get("/sign-up")
+    data = response.data.decode("utf-8")
+
+    # ASSERT
+    assert "Sign up" in data
